06846949cad2e0e6e642a7d6ea47ea177c19e510
Deepak Kumaravelu <deepak1122003kumar@gmail.com>
2024-03-24 21:39:25 +0530

    HelloController created

---
+++ src/main/java/com/deepakkumaravelu/taskmanager/HelloController.java
@@ -0,0 +1,12 @@
+package com.deepakkumaravelu.taskmanager;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello(){
+        return "hello world";
+    }
+}
